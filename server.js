b983eb2535fdd9f3752c2dfc51849cc6f6b27303
Chandler Beck <beckonator25@gmail.com>
2022-05-22 16:24:07 -0600

    added route to display

--- server.js
+++ server.js
@@ -18,11 +18,14 @@ app.use(express.static("public"));
 
 mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
-  useFindAndModify: false
+  useFindAndModify: false,
 });
 
 // routes
 app.use(require("./routes/api.js"));
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "./public/index.html"))
+);
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
